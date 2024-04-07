c2e742aa9cf2d8036d5d052c22591a9530c251e9
Aqib Akinyele <hackibkenn@gmail.com>
2024-04-07 22:53:15 +0100

    mobile almost

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -31,51 +31,47 @@ const Homepage = () => {
   };
 
   return (
-    <div className="dark:bg-brand-dark h-[100vh] md:h-[100vh] ">
-      <div className="md:h-[30vh] h-[50vh]">
+    <div className="dark:bg-brand-dark h-[150vh] md:h-[100vh] ">
+      <div className="md:h-[30vh] h-[28vh]">
         <SearchHeader reviewOnSubmit={reviewCallback} />
       </div>
 
       {reviewResult.length === 0 ? (
-        <div className="px-[100px] flex md:h-[70vh] justify-center items-center">
+        <div className="px-[100px] flex h-[50vh] md:h-[70vh] justify-center items-center">
           <div>
             <NoReview />
             <p className="mt-6 text-center">Oops! No review yet.</p>
           </div>
         </div>
       ) : (
-        <>
-          <div className="px-5 md:px-[100px] flex md:flex-row flex-col h-[50vh] md:h-[70vh]">
-            <div className="w-full md:w-3/5 overflow-auto">
-              <div className="grid grid-cols-1 gap-2 p-1">
-                {reviewResult.map((review, index) => (
-                  <>
-                    <ReviewContainer
-                      key={index}
-                      profileImage={<ProfileImage />}
-                      username={`${
-                        review.anonymous ? "Anonymous" : "James T."
-                      }`}
-                      datePosted={formatDateTime(new Date())}
-                      ratingImage={`⭐${review?.selectedStars}.0`}
-                      review={review.review}
-                    />
-                    <div className="border-[#D9D9D9] border"></div>
-                  </>
-                ))}
-              </div>
-            </div>
-
-            <div className="hidden md:block md:w-2/5 overflow-auto py-6">
-              <div className="flex flex-wrap gap-2">
-                <ImageOne />
-                <ImageTwo />
-                <ImageThree />
-                <ImageFour />
-              </div>
+        <div className="px-5 md:px-[100px] flex h-[50vh] md:h-[70vh]">
+          <div className="w-full md:w-3/5 overflow-auto">
+            <div className="grid grid-cols-1 gap-2 p-1">
+              {reviewResult.map((review, index) => (
+                <>
+                  <ReviewContainer
+                    key={index}
+                    profileImage={<ProfileImage />}
+                    username={`${review.anonymous ? "Anonymous" : "James T."}`}
+                    datePosted={formatDateTime(new Date())}
+                    ratingImage={`⭐${review?.selectedStars}.0`}
+                    review={review.review}
+                  />
+                  <div className="border-[#D9D9D9] border"></div>
+                </>
+              ))}
             </div>
           </div>
-        </>
+
+          <div className="hidden md:block md:w-2/5 overflow-auto py-6">
+            <div className="flex flex-wrap gap-2">
+              <ImageOne />
+              <ImageTwo />
+              <ImageThree />
+              <ImageFour />
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
